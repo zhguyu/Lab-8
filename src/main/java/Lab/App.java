dbd3f6cde812891b4d7e60ecb210a1e8ba091938
Guang Yu ZHANG <gzhangai@connect.ust.hk>
2019-04-11 13:43:38 +0800

    Task 2 Step 1.1 from Trump

--- src/main/java/Lab/App.java
+++ src/main/java/Lab/App.java
@@ -8,6 +8,7 @@ public class App {
         return "Hello world.";
     }
 
+	// Dummy comment
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
